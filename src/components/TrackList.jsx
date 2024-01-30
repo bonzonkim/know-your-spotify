bac0fb6275d0917aa88e77da3dfa46f40e80d459
Kelly Kang <flrhyme9@gmail.com>
2024-01-31 00:48:49 +0900

    fix: show all artists name

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -3,9 +3,9 @@ function TrackList({ topTrackData }) {
   <div>
       {topTrackData.map((track, index) => (
       <div key={index}>
-          <h3>{track.album.name}</h3>
-          <h3>{track.album.artists[0].name}</h3>
           <img src={track.album.images[1].url} alt={'artist image'}/>
+          <h3>{track.album.name}</h3>
+          {track.album.artists.map(artist => ( <p>{artist.name}</p> )) }
         </div>
       ))}
     </div>
