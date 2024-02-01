aa35d69932a770b873aef308cc38e29a24cb29bd
Kelly Kang <flrhyme9@gmail.com>
2024-02-01 15:50:19 +0900

    add: LoginButton, ListDiv

---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const ListDiv = styled.div`
+  padding-top: 10rem;
+  justify-content: center;
+  background-color: red;
+  align-items: center;
+  align-content: center;
+`
+
+export { ListDiv };
